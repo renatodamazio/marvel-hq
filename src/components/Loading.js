502b26df86b674d72dbc59570b9ceac9cc9d42ef
renato <dev-50dollar>
2021-04-14 13:01:06 -0300

    Updates

--- src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import loadingGif from '../images/load.gif';
-function Loading() {
-    return (
-        <div className="loading" style={{backgroundImage: `url(${loadingGif})`}}></div>
-    );
-}
-
-export default Loading;
